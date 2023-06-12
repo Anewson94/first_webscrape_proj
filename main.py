347068aa81c2f4760e6d692357ac8248b305a727
Anewson94 <dnewson59@gmail.com>
2023-06-12 18:02:37 -0400

    Title and transcript info

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 """
+Example with BeautifulSoup
 1. Install BeautifulSoup -- pip install beautifulsoup4
 2. Install requests -- pip install requests
 3. Install lxml -- pip install lxml
 """
+"""
+Reccommended order to find elements:
+1. By ID
+2. By class
+3. By tag, CSS Selector
+4. Xpath
+"""
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,3 +22,13 @@ result = requests.get(website)
 content = result.text
 # Content is a BeautifulSoup object after parsing
 soup = BeautifulSoup(content, "lxml")
+# print(soup.prettify())
+
+box = soup.find("article", class_="main-article")
+title = box.find("h1").get_text()
+
+transcript = soup.find("div", class_="full-script").get_text(strip=True, separator="\n")
+
+print(title)
+
+print(transcript)
